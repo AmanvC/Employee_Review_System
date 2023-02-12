3af804577b984e060eb6a6e827e1c533617badc5
Aman <amanvarshney.varshney@gmail.com>
2023-02-13 03:13:30 +0530

    Added styling to login and signup screens

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.urlencoded({extended: true}));
 
+const sassMiddleware = require('node-sass-middleware');
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}))
+
 app.use(express.static('assets'));
 
 app.use(session({
